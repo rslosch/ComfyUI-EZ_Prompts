8fb9b127f2d63389e8ba6182ff8d78072cf2e206
rslosch <slosch99@gmail.com>
2025-08-29 13:56:35 -0500

    fix import

--- __init__.py
+++ __init__.py
@@ -4,18 +4,18 @@ EZ Prompts - ComfyUI Template-based Prompt Generation
 
 from .nodes.ez_prompt_node import EZPromptsNode
 from .nodes.outpaint_by_aspect_ratio import PadImageForOutpaintByAspectRatio
-from .nodes.sort_batch_image_loader import LoadImageSetNodeSorted
+from .nodes.sort_batch_image_loader import LoadImageSetFromFolderSortedNode
 
 NODE_CLASS_MAPPINGS = {
     "EZPromptsNode": EZPromptsNode,
     "PadImageForOutpaintByAspectRatio": PadImageForOutpaintByAspectRatio,
-    "LoadImageSetNodeSorted": LoadImageSetNodeSorted
+    "LoadImageSetFromFolderSortedNode": LoadImageSetFromFolderSortedNode
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "EZPromptsNode": "EZ Prompts",
     "PadImageForOutpaintByAspectRatio": "Pad Image for Outpaint by Aspect Ratio",
-    "LoadImageSetNodeSorted": "Load Image Dataset (Sorted by Filename)"
+    "LoadImageSetFromFolderSortedNode": "Load Image Dataset from Folder (Sorted)"
 }
 
 WEB_DIRECTORY = "./js"
